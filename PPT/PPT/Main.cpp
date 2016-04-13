79f37f61cedbf46e0c587add7994e5d03768f326
Candelario Daniel Eguia <candelario.eguia@gmail.com>
2016-04-12 23:59:40 -0700

    Works except for the 3rd input trace... need an error catch

--- PPT/PPT/Main.cpp
+++ PPT/PPT/Main.cpp
@@ -14,9 +14,9 @@
 states	|	i	+	-	*	/	(	)	$
 E	0	|	TQ					TQ			
 Q	1	|		
-T	2	|	4	0	0	0	0	4	0	0
-R	3	|	0	9	9	5	6	0	9	9
-F	4	|	7	0	0	0	0	8	0	0
+T	2	|	
+R	3	|	
+F	4	|	
 
 
 */
@@ -30,9 +30,9 @@ int main()
 {
 	//table of the language
 	//the starting state is 0
-	string table[5][8] = { { "TQ",0,0,0,0,"TQ",0,0 },{ 0,"+TQ","-TQ",0,0,0,"L","L" },{ "FR",0,0,0,0,"FR",0,0 },{ 0,"L","L","*FR","-FR",0,"L","L" },{ "i",0,0,0,0,"(E)",0,0 } };
+	string table[5][8] = { { "QT","","","","","QT","","" },{ "","QT+","QT-","","","","L","L" },{ "RF","","","","","RF","","" },{ "","L","L","RF*","RF-","","L","L" },{ "i","","","","",")E(","","" } };
 	//w will hold the string of the individual words to check
-	string w, stack = "$0";
+	string w, stack = "$E";
 		char pop;
 	cout << "Enter a equation ending with '$' i.e. (i+i)*i$ : "; cin >> w;
 	//loop through the words in the text file
@@ -43,40 +43,48 @@ int main()
 		while (w[i] != '$')
 		{
 			//print the word letter by letter to prevent printing the '$' at the end
-			cout << w[i];
+			cout <<"Read: "<< w[i] << endl;
+			cout << "Stack\tPoped\tPushed\n";
+			cout << stack << "\t";
 			pop = stack.back();		// set pop to last char
 			stack.pop_back();		// remove last char
+			cout << pop << "\t";
 			while (pop != w[i]) {
-				switch (pop) {		// converts char to integer
-				case 'E':state = 0; break;	
-				case 'Q':state = 1; break;
-				case 'T':state = 2; break;
-				case 'R':state = 3; break;
-				case 'F':state = 4; break;
+				if (pop != 'L') {
+					switch (pop) {		// converts char to integer
+					case 'E':state = 0; break;
+					case 'Q':state = 1; break;
+					case 'T':state = 2; break;
+					case 'R':state = 3; break;
+					case 'F':state = 4; break;
+					}
+
+					switch (w[i])
+					{
+					case 'i':col = 0; break;
+					case '+':col = 1; break;
+					case '-':col = 2; break;
+					case '*':col = 3; break;
+					case '/':col = 4; break;
+					case '(':col = 5; break;
+					case ')':col = 6; break;
+					case '$':col = 7; break;
+					default: col = 8;		//is used for any letter not in the lanuage
+					}
+					//move to the new state
+					cout << table[state][col] << "\n";
+					stack += table[state][col];
 				}
-				
-				switch (w[i])
-				{
-				case 'i':col = 0; break;
-				case '+':col = 1; break;
-				case '-':col = 2; break;
-				case '*':col = 3; break;
-				case '/':col = 4; break;
-				case '(':col = 5; break;
-				case ')':col = 6; break;
-				case '$':col = 7; break;
-				default: col = 8;		//is used for any letter not in the lanuage
-				}
-				//move to the new state
-				stack += table[state][col];
+				cout << stack << "\t";
 				pop = stack.back();		// set pop to last char
 				stack.pop_back();		// remove last char
+				cout << pop << "\t";
 			}
-			cout << stack;
+			cout << stack << endl;
 			i++;
 		}
 		//output the result to the user
-		if (state == 4)
+		if (w[i]!='$' && pop != '$')
 			cout << " is not accepted\n";
 		else
 			cout << " is accepted\n";
