8162e04a28905a9bab6f5b762f0c68d52d4858cb
Candelario Daniel Eguia <candelario.eguia@gmail.com>
2016-04-13 12:55:09 -0700

    replaced char pop with stack.back()

--- PPT/PPT/Main.cpp
+++ PPT/PPT/Main.cpp
@@ -30,12 +30,12 @@ int main()
 {
 	//table of the language
 	//the starting state is 0
-	string table[5][8] = { { "QT","","","","","QT","","" },{ "","QT+","QT-","","","","L","L" },{ "RF","","","","","RF","","" },{ "","L","L","RF*","RF-","","L","L" },{ "i","","","","",")E(","","" } };
+	string table[5][8] = { { "QT","~","~","~","~","QT","~","~" },{ "~","QT+","QT-","~","~","~","L","L" },{ "RF","~","~","~","~","RF","~","~" },{ "~","L","L","RF*","RF-","~","L","L" },{ "i","~","~","~","~",")E(","~","~" } };
 	
 	char again;
 	do {
 		string w, stack = "$E";		//w will hold the string of the individual words to check
-		char pop;
+		//char pop;
 		cout << "Enter a equation ending with '$' i.e. (i+i)*i$ : "; cin >> w;
 		//loop through the words in the text file
 
@@ -48,12 +48,12 @@ int main()
 			cout << "\nRead: " << w[i] << endl;
 			cout << "Stack\tPoped\tPushed\n";
 			cout << stack << "\t";
-			pop = stack.back();		// set pop to last char
-			stack.pop_back();		// remove last char
-			cout << pop << "\t";
-			while (pop != w[i]) {
-				if (pop != 'L') {
-					switch (pop) {		// converts char to integer
+			//pop = stack.back();		// set pop to last char
+			//stack.pop_back();		// remove last char
+			cout << stack.back() << "\t";
+			while (stack.back() != w[i]) {
+				if (stack.back() != 'L') {
+					switch (stack.back()) {		// converts char to integer
 					case 'E':state = 0; break;
 					case 'Q':state = 1; break;
 					case 'T':state = 2; break;
@@ -75,20 +75,29 @@ int main()
 					}
 					//move to the new state
 					cout << table[state][col] << "\n";
+					stack.pop_back();
 					stack += table[state][col];
 					cout << stack << "\t";
 				}
-				else { cout << endl << stack << '\t'; }
+				
+				else {
+					stack.pop_back();
+					cout << endl << stack << '\t'; 
+				}
 
-				pop = stack.back();		// set pop to last char
-				stack.pop_back();		// remove last char
-				cout << pop << "\t";
+				//pop = stack.back();		// set pop to last char
+				//stack.pop_back();		// remove last char
+				cout << stack.back() << "\t";
+				if (stack.back() == '~')break;
 			}
+			if (stack.back() == '~')break;
+			stack.pop_back();
+			
 			cout << stack << endl;
 			i++;
 		}
 		//output the result to the user
-		if (w[i] != '$' && pop != '$')
+		if (w[i] != '$' && stack.back() != '$')
 			cout << " is not accepted";
 		else
 			cout << " is accepted";
